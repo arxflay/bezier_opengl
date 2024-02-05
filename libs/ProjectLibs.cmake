84156335ba57c544732f3f8a9ff418f9a99ed3e9
arxflay <arxflay@yandex.ru>
2024-02-05 20:22:17 +0100

    Fix compilation

--- libs/ProjectLibs.cmake
+++ libs/ProjectLibs.cmake
@@ -7,7 +7,7 @@ endif()
 set(CPM_SOURCE_CACHE ${CMAKE_CURRENT_LIST_DIR}/third_party/CPM)
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/FetchCPM.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/CPM/CPM.cmake)
-CPMAddPackage("gh:g-truc/glm#0.9.9.8")
+CPMAddPackage("gh:g-truc/glm#0.9.9.4")
 CPMAddPackage(NAME glfw3
     GITHUB_REPOSITORY glfw/glfw
     VERSION 3.3.1
